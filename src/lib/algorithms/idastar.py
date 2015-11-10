ef728d80d71f1889e06f9a103fef37d6fb83c858
Marco Zanella <mz@openmailbox.org>
2015-11-10 23:44:44 +0100

    Proudly added IDA*

--- src/lib/algorithms/idastar.py
+++ src/lib/algorithms/idastar.py
@@ -12,8 +12,8 @@ def one(a=None, b=None):
     return 1
 
 
-def search(start, is_goal, s, h, cost=one):
-    """ Performs an A* search.
+def search(start, is_goal, s, h, cost=one, max_depth=1):
+    """ Performs an A* search up to given depth level.
 
     Args:
         start (hashable): Node to start the search from
@@ -21,35 +21,61 @@ def search(start, is_goal, s, h, cost=one):
         successor (hashable -> [hashable]: returns successors of a node
         heuristic (hashable -> int): estimates a cost to a node
         cost (hashable, hashable -> int): cost to move between nodes
+        max_depth (int): maximum depth for this iteration
+
+    Returns:
+        [hashable]: sequence of states from start to goal
+    """
+    path = []
+    while not path:
+        path = limited_search(start, is_goal, s, h, cost, max_depth)
+        max_depth = max_depth + 1
+    return path
+
+
+def limited_search(start, is_goal, s, h, cost=one, max_depth=1):
+    """ Performs an A* search up to given depth level.
+
+    Args:
+        start (hashable): Node to start the search from
+        is_goal (hashable -> bool): tells whether a node is a goal node
+        successor (hashable -> [hashable]: returns successors of a node
+        heuristic (hashable -> int): estimates a cost to a node
+        cost (hashable, hashable -> int): cost to move between nodes
+        max_depth (int): maximum depth for this iteration
 
     Returns:
         [hashable]: sequence of states from start to goal
     """
     frontier = []
     parents = {}
+    depths = {}
     g = {}
 
     heapq.heappush(frontier, (1, start))
     parents[start] = None
+    depths[start] = 0
     g[start] = 0
-    e = 1
 
     while frontier:
         (priority, node) = heapq.heappop(frontier)
-        e = e + 1
 
         if is_goal(node):
-            print(e)
             return backtrack(node, parents)
+
+        if depths[node] > max_depth:
+            continue
 
         successors = s(node)
         for next_node in successors:
             new_cost = g[node] + cost(node, next_node)
             if next_node not in g or new_cost < g[next_node]:
                 parents[next_node] = node
+                depths[next_node] = depths[node] + 1
                 g[next_node] = new_cost
                 priority = g[next_node] + h(next_node)
                 heapq.heappush(frontier, (priority, next_node))
+    return []
 
 
 def backtrack(node, parents):
