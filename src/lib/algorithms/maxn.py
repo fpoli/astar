02a50645ac68acf887e1846bf10ddb556f69a289
erpicci <mz@openmailbox.org>
2015-11-11 15:05:59 +0100

    Proudly implemented MaxN

--- src/lib/algorithms/maxn.py
+++ src/lib/algorithms/maxn.py
@@ -1,18 +1,47 @@
 # -*- coding: UTF-8 -*-
 
-def maxn(node, successors, evaluation, player, max_players):
-    if not successors(node):
-        return evaluation(node)
 
-    next_player = (player + 1) % max_players
-    nodes = successors(node)
-    children = [ maxn(child, successors, evaluation, next_player, max_players) for child in nodes ]
-    return choose(children, player)
+def maxn(root, s, payoff, player, players):
+    """ Performs the MaxN search.
+
+    Implements the original MaxN search algorithm by Luckhardt and Irani
+    (for info see "An Algorithmic Solution of N-Person Games",
+    Luckhardt-Irani, 1986).
+
+    Args:
+        root (status): Root node of the search
+        s (status -> [(status, action)]): returns successors of a status
+        payoff (status -> (int*)): returns a tuple of payoffs
+        player (int): player whose payoff shall be maximized
+        players (int): number of players
+
+    Returns:
+        (status, [action]): Best status for given player and sequence of
+        actions to reach it
+    """
+    if not s(root):
+        return (payoff(root), [])
+
+    next_player = (player + 1) % players
+    children = []
+    for (successor, action) in s(root):
+        (status, actions) = maxn(successor, s, payoff, next_player, players)
+        children.append((status, [action] + actions))
+    return best(children, player)
 
 
-def choose(children, player):
-    best = children[0]
+def best(children, player):
+    """ Returns best move for given player.
+
+    Args:
+        children ([(status, [actions])]): children to choose from
+        player (int): player whose payoff shall be maximized
+
+    Returns:
+        (status, [actions]): child with best payoff
+    """
+    best_child = children[0]
     for child in children:
-        if child[player] > best[player]:
-            best = child
-    return best
+        if child[0][player] > best_child[0][player]:
+            best_child = child
+    return best_child
