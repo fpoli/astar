3a1ef3bbf4b16729a2754f2613e48e5cd0244ad5
Federico Poli <federpoli@gmail.com>
2015-11-19 23:32:14 +0100

    Test MaxnBot's execution time

--- test/lib/bots/maxn_test.py
+++ test/lib/bots/maxn_test.py
@@ -2,9 +2,11 @@
 
 import unittest
 from test import generate_tests
-from status_samples import get_status_samples_dict
+from status_samples import get_status_samples, get_status_samples_dict
 from lib.models import Map, Status, Action
 from lib.bots import MaxnBot
+from timeit import Timer
+from random import choice
 
 
 class TestMaxnBotAction(unittest.TestCase):
@@ -34,3 +36,23 @@ class TestMaxnBotAction(unittest.TestCase):
         action = bot.think(status)
 
         self.assertEqual(action, Action.east)
+
+    def test_bot_execution_time(self):
+        """Test that the bot's execution time is not too big"""
+
+        # Build models
+        samples_dict = get_status_samples()
+        samples_status = []
+        for status_dict in samples_dict:
+            map_obj = Map(status_dict["game"]["board"]["tiles"])
+            status = Status(status_dict["game"], map_obj)
+            samples_status.append(status)
+
+        bot = MaxnBot()
+        t = Timer(lambda: bot.think(choice(samples_status)))
+
+        # We are interested in the maximum execution time, not in the average!
+        # This, obviously, depends on the system used for testing.
+        execution_time = max(t.repeat(repeat=3, number=1))
+
+        self.assertLessEqual(execution_time, 0.9)
