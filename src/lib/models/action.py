26be0cc5a0c36f4591078cbaad45d98325d6e078
Federico Poli <federpoli@gmail.com>
2015-12-02 14:26:20 +0100

    Move immutable dicts outside functions in action.py

--- src/lib/models/action.py
+++ src/lib/models/action.py
@@ -4,26 +4,28 @@ from enum import Enum
 
 Action = Enum("Action", "north south west east stay")
 
+__str_to_action_conversion = {
+    "North": Action.north,
+    "South": Action.south,
+    "East": Action.east,
+    "West": Action.west,
+    "Stay": Action.stay
+}
+
+__action_to_dir_conversion = {
+    Action.north: (0, -1),
+    Action.south: (0, 1),
+    Action.west: (-1, 0),
+    Action.east: (1, 0),
+    Action.stay: (0, 0)
+}
+
 
 def str_to_action(s):
-    conversion = {
-        "North": Action.north,
-        "South": Action.south,
-        "East": Action.east,
-        "West": Action.west,
-        "Stay": Action.stay
-    }
-    assert(s in conversion)
-    return conversion[s]
+    assert(s in __str_to_action_conversion)
+    return __str_to_action_conversion[s]
 
 
 def action_to_dir(a):
-    conversion = {
-        Action.north: (0, -1),
-        Action.south: (0, 1),
-        Action.west: (-1, 0),
-        Action.east: (1, 0),
-        Action.stay: (0, 0)
-    }
-    assert(a in conversion)
-    return conversion[a]
+    assert(a in __action_to_dir_conversion)
+    return __action_to_dir_conversion[a]
